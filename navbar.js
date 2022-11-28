51c8cd95bd443a9d320643429fb4cc2d99866d37
sukhchain77 <sukhchaink317@gmail.com>
2022-11-28 22:36:51 +0530

    firstupload

---
+++ navbar.js
@@ -0,0 +1,51 @@
+let navbar = () =>{
+    return ` 
+    <div id="logo-container">
+    <div class="logo_head">
+    <div class="fc_logo">
+        <a href=""><img class="header_logo" src="https://cdn.fcglcdn.com/brainbees/images/n/fc_logo.png" alt="logoimg"></a>
+    </div>
+    <div class="fc_search">
+        <form class="headSearchBox">
+            <input type="text" name="" id="search_box" placeholder="Search for a Category, Brand or Product">
+            <span class="search-button"><i class="fa fa-magnifying-glass"></i></span>
+        </form>
+    </div>
+    <div class="fc_login">
+       <ul>
+        <li><i class="fa fa-location-dot"></i><span>  Select location</span></li>
+        <li><span>Stores & Preschools</span></li>
+        <li><span>Support</span></li>
+        <li><span>Track Order</span></li>
+        <li><span>FirstCry Parenting</span></li>
+        <li><span>Login /</span><span> Register</span></li>
+        <li><i class="fa-regular fa-heart"></i><span>  Shortlist</span></li>
+        <li class="midicon"><i class="fa-solid fa-cart-shopping"></i></i><span class="cart_c">Cart</span></li>
+       </ul>
+    </div>
+    </div>
+    </div>
+    <div class="menu-container">
+        <ul id="navlink">
+            <li class="M14_42"><a href=""> All Categories<span><i class="fa fa-angle-down"></i></span></a></li>
+            <li class="M14_42"><a href=""> BOY FASHION</a></li>
+            <li class="M14_42"><a href=""> GIRL FASHION</a></li>
+            <li class="M14_42"><a href=""> Footwear</a></li>
+            <li class="M14_42"><a href=""> Toys</a></li>
+            <li class="M14_42"><a href=""> Diapering</a></li>
+            <li class="M14_42"><a href=""> Gear</a></li>
+            <li class="M14_42"><a href=""> Feeding</a></li>
+            <li class="M14_42"><a href=""> Bath</a></li>
+            <li class="M14_42"><a href=""> Nursery</a></li>
+            <li class="M14_42"><a href=""> Moms</a></li>
+            <li class="M14_42"><a href=""> Health</a></li>
+            <li class="M14_42"><a href=""> Boutiques</a></li>
+            <li class="M14_42"><a href=""><img src="https://cdn.fcglcdn.com/brainbees/images/n/club_logo.png"></a></li>
+            <li class="M14_42 carters"><a href=""><img src="https://cdn.fcglcdn.com/brainbees/banners/FC-menu-carters-logo.webp"></a></li>
+        </ul>
+
+    </div>`
+}
+
+
+export {navbar};
