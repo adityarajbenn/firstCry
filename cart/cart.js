d6476ca15e713944981e0006d6da789b015de163
Aljith KJ <kwdeathlok@gmail.com>
2022-11-30 15:09:34 +0530

    Cart page 90% Completed

--- cart/cart.js
+++ cart/cart.js
@@ -1,14 +1,15 @@
 
-let total = 0;
-let discount = 0;
-let realPrice = 0;
-
 function display(data){
+
     let cartProducts = document.getElementById('cartProducts');
     cartProducts.innerHTML = null;
     console.log(data);
-    data.map((elem)=> {
 
+    let total = 0;
+    let discount = 0;
+    let realPrice = 0;
+
+    data.map((elem, i)=> {
       let curTotal = elem.strikePrice * (elem.qty || 1);
       total += curTotal;
 
@@ -101,10 +102,37 @@ function display(data){
                       op5.innerText = 5;
                 
                 select.append(op1, op2, op3, op4, op5);
-
-                // select.value = `${op${}.innerText}`; 
+                if(elem.qty == null) elem.qty = 1;
+                select.value = elem.qty;
+              
                 select.onchange = ()=> {
-                    console.log(select.value)
+                    let val = select.value;
+                    if(val == 1){
+                        elem.qty = op1.innerText;
+                        localStorage.setItem('cart', JSON.stringify(data));
+                        display(data);
+                    } 
+                    if(val == 2){
+                        elem.qty = op2.innerText;
+                        localStorage.setItem('cart', JSON.stringify(data));
+                        display(data);
+                    } 
+                    if(val == 3){
+                        elem.qty = op3.innerText;
+                        localStorage.setItem('cart', JSON.stringify(data));
+                        display(data);
+                    }
+                    if(val == 4){
+                        elem.qty = op4.innerText;
+                        localStorage.setItem('cart', JSON.stringify(data));
+                        display(data);
+                    }
+                    if(val == 5){
+                        elem.qty = op5.innerText;
+                        localStorage.setItem('cart', JSON.stringify(data));
+                        display(data);
+                    } 
+                    
                 }
               qtyDiv.append(qtyP, select);
 
@@ -118,6 +146,11 @@ function display(data){
             let sDiv1 = document.createElement('div');
               let delBtn = document.createElement('button');
               delBtn.innerHTML = '<span class="material-symbols-outlined">delete</span>REMOVE';
+              delBtn.onclick = ()=> {
+                  data.splice(i, 1);
+                  localStorage.setItem('cart', JSON.stringify(data));
+                  display(data);
+              }
             sDiv1.append(delBtn);
             let sDiv2 = document.createElement('div');
               let listBtn = document.createElement('button');
@@ -131,9 +164,269 @@ function display(data){
 
     document.getElementById('strikeTot').textContent = `₹${total}`;
     document.getElementById('discTot').textContent = `₹${discount}`;
-    document.getElementById('subTot').textContent = `₹${realPrice - 77}`;
-    document.getElementById('final').textContent = `₹${realPrice - 77}`; 
+    document.getElementById('subTot').textContent = `₹${realPrice + 77}`;
+    document.getElementById('final').textContent = `₹${realPrice + 77}`; 
+    document.getElementById('totalPrice').textContent = `₹${realPrice + 77}`; 
+    document.getElementById('payTotal').textContent = `₹${realPrice + 77}`; 
+    document.getElementById('noProd').textContent = data.length; 
+
+    if(data.length == 0){
+        document.getElementById('subTot').textContent = '₹0';
+        document.getElementById('final').textContent = '₹0'; 
+        document.getElementById('charge').textContent = '₹0'; 
+        document.getElementById('gst').textContent = '₹0'; 
+        document.getElementById('totalPrice').textContent = '₹0'; 
+    }
+    localStorage.setItem('total', (realPrice + 77));
 }
 
-let data = JSON.parse(localStorage.getItem('cart'));
+let data = JSON.parse(localStorage.getItem('cart')) || [];
 display(data);
+
+let dark = document.getElementById('dark');
+let payMethodDiv = document.getElementById('payMethod');
+let close = document.getElementById('close');
+close.onclick = ()=> {
+    payMethodDiv.style = "display: none";
+    dark.style = "display: none";
+}
+
+let orderBtn = document.getElementById('order');
+order.onclick = ()=> {
+    payMethodDiv.style = "display: block";
+    dark.style = "display: block";
+}
+
+let expand = document.getElementById('expand');
+let arrow = document.getElementById('arrow');
+arrow.onclick = ()=> {
+
+    if(arrow.innerText == 'expand_less'){
+        arrow.innerText = 'expand_more';
+        expand.style = "display: none;";
+    }else{
+        arrow.innerText = 'expand_less';
+        expand.style = "display: block;";
+    }
+}
+
+
+// SUGGESTION PRODUCTS
+
+let suggestProducts = [
+  {
+    id: 21,
+    url1: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11550559a.webp",
+    url2: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11550559b.webp",
+    url3: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11550559c.webp",
+    img1: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11550555a.webp",
+    img2: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11550555b.jpg",
+    img3: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11550555c.jpg",
+    title: "Babyhug Full Sleeves Cotton T-Shirt Camouflage Print - Blue",
+    desc: "Comfy & stylish t-shirt with round neck & rib at neck for boys",
+    price: 275,
+    strikePrice: 349,
+    off: 21,
+    delivery: "Get it by Monday, Dec 17",
+    color: "blue",
+  },
+  {
+    id: 22,
+    url1: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11525689a.webp",
+    url2: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11525689b.webp",
+    url3: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11525689c.webp",
+    img1: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11525683a.webp",
+    img2: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11525683b.jpg",
+    img3: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11525683c.jpg",
+    title: "Babyhug Full Sleeves T-Shirts Simba Print - White",
+    desc: "Comfortable round neck t-shirts with shoulder buttons for boys",
+    price: 394,
+    strikePrice: 499,
+    off: 21,
+    delivery: "Get it by Tuesday, Dec 14",
+    color: "white",
+  },
+  {
+    id: 23,
+    url1: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11398383a.webp",
+    url2: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11398383b.webp",
+    url3: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11398383c.webp",
+    img1: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11398385a.webp",
+    img2: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11398385b.jpg",
+    img3: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11398385c.jpg",
+    title: "Babyhug Full Sleeves Striped Skivi Tee - Red",
+    desc: "Snug turtle neck pullover style tee for boys",
+    price: 394,
+    strikePrice: 499,
+    off: 21,
+    delivery: "Get it by Tuesday, Dec 17",
+    color: "red",
+  },
+  {
+    id: 24,
+    url1: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11493273a.webp",
+    url2: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11493273b.webp",
+    url3: "https://cdn.fcglcdn.com/brainbees/images/products/438x531/11493273c.webp",
+    img1: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11493270a.webp",
+    img2: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11756098b.jpg",
+    img3: "https://cdn.fcglcdn.com/brainbees/images/products/300x364/11493270c.jpg",
+    title:
+      "Pine Kids Cotton Knit Full Sleeves Racetrack Printed Biowash T Shirt - Yellow",
+    desc: "Comfy round neck tshirt with rib at neck for boys",
+    price: 473,
+    strikePrice: 599,
+    off: 21,
+    delivery: "Get it by Tuesday, Dec 22",
+    color: "yellow",
+  }
+];
+
+displayProducts(suggestProducts);
+function displayProducts(data){
+  let productDiv = document.getElementById("productDiv");
+  productDiv.innerHTML = null;
+
+
+  data.map(({id, img1, img2, img3, title, desc, price, strikePrice, off, delivery, url1, url2, url3, color})=> {
+      let div = document.createElement("div");
+      div.setAttribute("class", "products");
+
+      let imgDiv = document.createElement("div");
+        let imgP = document.createElement("img");
+        imgP.src = img1;
+      imgDiv.append(imgP);
+
+      let titleP = document.createElement("p");
+        titleP.innerText = title;
+      
+      let strikeDiv = document.createElement("div");
+      strikeDiv.setAttribute("class", "strikePriceDiv");
+      
+        let priceP = document.createElement("p");
+          priceP.innerText = `₹ ${price}`;
+        let strikeP = document.createElement("span");
+          strikeP.innerText = `₹ ${strikePrice}`;
+        let offP = document.createElement("span");
+          offP.innerText = `(${off}% Off)`;
+      strikeDiv.append(priceP, strikeP, offP);
+
+      let priceDiv = document.createElement("div");
+      priceDiv.setAttribute("class", "priceDiv");
+
+        let starDiv = document.createElement("div");
+          let starImg = document.createElement("img");
+          starImg.src = "./img/star.png";
+        starDiv.append(starImg);
+
+        let clubP = document.createElement("p");
+          clubP.innerHTML = `Club Price: <span>₹ ${price - 20}</span>`;
+        let symbol = document.createElement("span");
+          symbol.setAttribute("class", "material-symbols-outlined");
+          symbol.innerText = "chevron_right";
+
+      priceDiv.append(starDiv, clubP, symbol);
+
+      let p = document.createElement("p");
+        p.innerText = delivery;
+
+      let cartDiv = document.createElement("div");
+        cartDiv.setAttribute("class", "cartDiv");
+      
+        let btnDiv = document.createElement("div");
+          
+          let btn1 = document.createElement("button");
+            btn1.innerText = "ADD TO CART";
+            btn1.onclick = ()=>{
+
+                let obj = new dataObj(id, url1, url2, url3, img1, title, desc, price, strikePrice, delivery, color, off);
+                let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+                let flag = true;
+                cart.forEach((elem)=> {
+                    if(elem.id == id) flag = false;
+                })
+
+                if(flag) {
+                    cart.push(obj);
+                    localStorage.setItem("cart", JSON.stringify(cart));
+                    display(cart);
+                }
+                
+                // console.log(obj);
+            };
+          let btn2 = document.createElement("button");
+            btn2.innerText = "SHORTLIST";
+            btn2.onclick = ()=>{
+
+                let obj = new dataObj(id, url1, url2, url3, img1, title, desc, price, strikePrice, delivery, color, off);
+                let shortList = JSON.parse(localStorage.getItem("shortList")) || [];
+
+                let flag = true;
+                shortList.forEach((elem)=> {
+                    if(elem.id == id) flag = false;
+                })
+
+                if(flag) {
+                    shortList.push(obj);
+                    localStorage.setItem("shortList", JSON.stringify(shortList));
+                }
+
+                // console.log(obj);
+            };
+        btnDiv.append(btn1, btn2);
+      cartDiv.append(btnDiv);
+      let _id;
+      div.addEventListener("mouseenter", ()=> {
+          let arr = [img1, img2, img3];
+          let k = 1;
+          _id = setInterval(() => {
+              imgP.src = arr[k%3];
+              k++;
+          }, 1000);
+      });
+
+      div.addEventListener("mouseleave", ()=> {
+          clearInterval(_id);
+          imgP.src = img1;
+      })
+
+      div.append(imgDiv, titleP, strikeDiv, priceDiv, p, cartDiv);
+      productDiv.append(div);
+      
+      imgDiv.onclick = ()=> {
+          let obj = new dataObj(id, url1, url2, url3, img1, title, desc, price, strikePrice, delivery, color);
+          localStorage.setItem("product", JSON.stringify(obj));
+          window.location.href = "details.html";
+      }
+  })
+}
+
+function dataObj(id, url1, url2, url3, img1, title, desc, price, strikePrice, delivery, color, off){
+  this.id = id;
+  this.img1 = url1;
+  this.img2 = url2;
+  this.img3 = url3;
+  this.cartImg = img1;
+  this.title = title;
+  this.desc = desc;
+  this.price = price;
+  this.strikePrice = strikePrice;
+  this.delivery = delivery;
+  this.color = color;
+  this.off = off; 
+}
+
+// COUPON 
+let couponCode = document.getElementById('couponCode');
+let apply = document.getElementById('apply');
+
+apply.onclick = ()=> {
+    let amount = localStorage.getItem('total');
+    if(couponCode.value == 'masai30'){
+        amount = ((amount*70) / 100).toFixed(2);
+        document.getElementById('totalPrice').textContent = `₹${amount}`; 
+        document.getElementById('payTotal').textContent = `₹${amount}`; 
+    }
+    couponCode.value = null;
+}
+
