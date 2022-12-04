2efe6d645839af1e096a7f31ea18e1d1cd26e1f3
Aljith KJ <kwdeathlok@gmail.com>
2022-12-04 21:13:45 +0530

    Json server link updated

--- products/utils/fetch.js
+++ products/utils/fetch.js
@@ -1,6 +1,6 @@
 async function fetchData(){
     try{
-        let res = await fetch('https://mock-data-server.onrender.com/data');
+        let res = await fetch('https://mock-data-server-l9md.onrender.com/data');
         return await res.json();
     }catch(e){
         console.log(e);
